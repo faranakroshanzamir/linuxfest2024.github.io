ab8603ff2539624c866b060006f5e0dc2e102cb1
F88 <133592572+faranakroshanzamir@users.noreply.github.com>
2023-07-30 12:37:25 -0400

    Add files via upload

---
+++ src/components/NotificationsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <notifications group="auth" position="top center" />
+</template>
+
+<script>
+export default {
+  name: 'NotificationsContainer',
+}
+</script>
+
+<style scoped>
+.notification-title {
+  direction: rtl !important;
+  text-align: right;
+}
+
+.notification-content {
+  direction: rtl !important;
+  text-align: right;
+}
+</style>
